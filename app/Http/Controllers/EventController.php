18d8d5bb478f3e34e7960b6736e062b73224231f
geovandp <giovan472@gmail.com>
2024-05-13 23:06:22 +0700

    membuat form create event, fungsional create dan destroy event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    function index() {
+        // $event = Event::all();
+        // return view();
+    }
+
+    function search(Request $request) {
+        // $event = Event::all();
+        // $search = $request->search;
+        // $event = event::where('name_event', 'LIKE', '%'.$search.'%')->get();
+        // return view('event/index', ['eventList' => $event]);
+    }
+
+    function create()
+    {
+        return view('create-event');
+    }
+
+    function store(Request $request)
+    {
+        $event = new Event;
+        $event->nama_event = $request->nama_event;
+        $event->tanggal_mulai = $request->tanggal_mulai;
+        $event->tanggal_selesai = $request->tanggal_selesai;
+        $event->description_event = $request->description_event;
+        $event->save();
+        return redirect('/event');
+    }
+
+    function show()
+    {
+
+    }
+
+    function edit()
+    {
+
+    }
+
+    function update()
+    {
+        
+    }
+
+    function destroy(Event $event) {
+        $event->delete();
+        return redirect('/event');
+    }
 }
--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -9,5 +9,6 @@
     <h1>Ini Landing Page</h1>
     <a class="btn btn-warning" href="/account/create" role="button">Register</a>
     <a class="btn btn-warning" href="/account/login" role="button">Login</a>
+    <a class="btn btn-warning" href="/event/create" role="button">Create Event</a>
 
 @endsection
